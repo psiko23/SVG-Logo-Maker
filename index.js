60d7f07dc24d4155b4278d5d3dc2330324039bd3
Psiko23 <tylerpittman2001@gmail.com>
2024-01-04 15:42:40 -0800

    svg generation

---
+++ index.js
@@ -0,0 +1,72 @@
+import inquirer from 'inquirer';
+import fs from 'fs';
+import { Triangle, Circle, Square } from './lib/shapes.js';
+
+function getUserInput() {
+    inquirer.prompt([
+        {
+            name: 'logoText',
+            message: 'What text should be included? (up to three characters)',
+            type: 'input'
+        },
+        {
+            name: 'textColor',
+            message:'What color should the text be? (enter a color keyword or hexadecimal number)',
+            type: 'input'
+        },
+        {
+            name: 'shape',
+            message: 'What shape do you want your logo to be?',
+            type: 'list',
+            choices: ['triangle','circle','square']
+        },
+        {
+            name: 'shapeColor',
+            message: 'What color should the shape be? (enter a color keyword or hexadecimal number)',
+            type: 'input'
+        }
+    ])
+    .then(answers => {
+        console.log('Answers:  ' ,answers);
+        generateSVG('output.svg', answers)
+    })
+}
+
+getUserInput();
+
+function generateSVG(fileName, answers) {
+
+    let svgString = '';
+
+    let shapeChoice;
+
+    if (answers.shape === 'triangle') {
+
+        shapeChoice = new Triangle();
+
+    } else if (answers.shape === 'circle') {
+
+        shapeChoice = new Circle();
+
+    } else {
+
+        shapeChoice = new Square();
+
+    }
+
+    const userText = answers.logoText;
+    const textColor = answers.textColor 
+    shapeChoice.setColor(answers.shapeColor);
+
+    svgString += shapeChoice.render(answers.logoText, answers.textColor)
+    svgString += `<text x="50%" y="50%" font-size="40" text-anchor="middle" dy=".3em" fill="${textColor}">${userText}</text></g></svg>`
+    console.log(svgString);
+
+    fs.writeFile('output.svg', svgString, (err) => {
+        if (err) {
+            console.error('Error writing SVG file: ', err);
+        } else {
+            console.log('SVG file generated! ', 'output.svg')
+        }
+    })
+}
